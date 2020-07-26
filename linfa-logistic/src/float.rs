a4aa911b4b5c7cbda8e1144aaf364e596b8b867a
Lorenz Schmidt <bytesnake@mailbox.org>
2020-07-26 19:08:31 +0200

    Remove FloatWrapper and fix types in tests

--- linfa-logistic/src/float.rs
+++ linfa-logistic/src/float.rs
@@ -1,13 +1,27 @@
-use argmin::prelude::ArgminFloat;
+use argmin::prelude::{ArgminFloat, ArgminMul};
 use ndarray::NdFloat;
+use ndarray::LinalgScalar;
 use ndarray_linalg::Lapack;
 use num_traits::FromPrimitive;
+use crate::ArgminParam;
 
 /// A Float trait that captures the requirements we need for the various
 /// places we use floats. These are basically imposed by NdArray and Argmin.
-pub trait Float: ArgminFloat + NdFloat + Lapack + Default + Clone + FromPrimitive {
+pub trait Float: ArgminFloat + NdFloat + Lapack + Default + Clone + FromPrimitive + LinalgScalar + ArgminMul<ArgminParam<Self>, ArgminParam<Self>> {
     const POSITIVE_LABEL: Self;
     const NEGATIVE_LABEL: Self;
+}
+
+impl ArgminMul<ArgminParam<f32>, ArgminParam<f32>> for f32 {
+    fn mul(&self, other: &ArgminParam<f32>) -> ArgminParam<f32> {
+        ArgminParam(&other.0 * *self)
+    }
+}
+
+impl ArgminMul<ArgminParam<f64>, ArgminParam<f64>> for f64 {
+    fn mul(&self, other: &ArgminParam<f64>) -> ArgminParam<f64> {
+        ArgminParam(&other.0 * *self)
+    }
 }
 
 impl Float for f32 {
